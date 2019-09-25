440b81fab87f6e23269a9e2ab01aa8383c73d53b
John <jschriem@gmail.com>
2019-09-24 21:42:05 -0700

    dark mode? lol

--- my-app/src/DeckoCards.js
+++ my-app/src/DeckoCards.js
@@ -9,7 +9,8 @@ import './DeckoCards.css';
 export default class DeckoCards extends React.Component{
     render(){
         return(
-<CardDeck style={{display: 'flex', flexDirection: 'row', width: '70rem'}}>
+          <div className = 'body'>
+<CardColumns style={{display: 'flex', flexDirection: 'row', width: '70rem'}}>
   <Card>
     <Card.Img variant="top" src={ require('./images/tennisRadar.png') } />
     <Card.Body>
@@ -46,7 +47,47 @@ export default class DeckoCards extends React.Component{
       <small className="text-muted">Source:  kijiji.ca </small>
     </Card.Footer>
   </Card>
-</CardDeck>
+</CardColumns>
+
+<CardColumns style={{display: 'flex', flexDirection: 'row', width: '70rem'}}>
+  <Card>
+    <Card.Img variant="top" src={ require('./images/Total_USopen.png') } />
+    <Card.Body>
+    <Card.Title>Total US Open Wins in the Open Era</Card.Title>
+    <Card.Text>
+      Trying out a new graph to visualize the total amount of grand slam wins at flushing meadows
+    </Card.Text>
+  </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Source:  usopen.org </small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src={ require('./images/Sunrun2018.png') } />
+    <Card.Body>
+    <Card.Title>Personal Running Data</Card.Title>
+    <Card.Text>
+      Tracked paces and times for runs betwen the 2018 and 2019 Sun Run in Vancouver
+    </Card.Text>
+  </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Source:  Nike+ Training Data </small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src={ require('./images/vicHousing.png') } />
+    <Card.Body>
+    <Card.Title>Victoria Rental Prices</Card.Title>
+    <Card.Text>
+      Web-scraped rental prices rentals in the Victoria BC area to compare prices per bedrooms
+    </Card.Text>
+  </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Source:  kijiji.ca </small>
+    </Card.Footer>
+  </Card>
+</CardColumns>
+</div>
         );
     }
 }
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-const green = '#2ECC71';
+const green = 'teal';
 const gold = '#FFC300';
 
 export default class Header extends React.Component{
@@ -20,35 +20,35 @@ export default class Header extends React.Component{
 
   changeColor(){
     const newColor = this.state.color == green ? gold : green;
-  this.setState({color: newColor});
+    this.setState({color: newColor});
   }
 
     render(){
         return(
-          <nav class="navbar navbar-expand-lg" style ={{background: this.state.color}}>
-  <a class="navbar-brand" href="#"></a>
-  <img src={ require('./images/swords.png') } />
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Data1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Data2</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Data3</a>
-      </li>
-    </ul>
-    <button className = 'btn' onClick = {this.changeColor} style = {{float: 'right'}}> </button>
-  </div>
-</nav>
+  <nav class="navbar navbar-expand-lg" style ={{background: this.state.color}}>
+    <a class="navbar-brand" href="#"></a>
+    <img src={ require('./images/swords.png') } />
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tennis</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Health</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Extra</a>
+        </li>
+      </ul>
+      <button className = 'btn' onClick = {this.changeColor} style = {{float: 'right'}}> </button>
+    </div>
+  </nav>
         );
     }
 }
